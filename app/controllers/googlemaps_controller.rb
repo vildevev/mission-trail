a69e3f7d5c6e25558b0979f96c9b6f7287b7ba1e
“Vilde <vilde@vevatne.no>
2017-04-15 18:49:56 -0700

    Removes unneccessary comments in googlemaps controller

--- app/controllers/googlemaps_controller.rb
+++ app/controllers/googlemaps_controller.rb
@@ -12,27 +12,6 @@ class GooglemapsController < ApplicationController
     @event = @event_array.sample
     @event.update_attributes(seen?: true)
     redirect_to googlemap_path(@event)
-
-
-
-    # @num = rand(50)
-    # p @num
-    # if @num >= 40
-    #   @event = Event.find_by(name: "good-map")
-    #   @attr_change = @event.attr_change
-    #   gon.gametext = @event.text
-    # else
-    #   @event = Event.find_by(name: "bad-map")
-    #   @attr_change = @event.attr_change
-    #   gon.gametext = @event.text
-    # end
-    # gon.username = @game.username
-    # gon.fomo = @game.fomo
-    # gon.battery = @game.battery
-    # gon.time = @game.time
-    # gon.money = @game.money
-    # gon.gameimage = "/assets/googlemaps.png"
-
   end
 
   def show
